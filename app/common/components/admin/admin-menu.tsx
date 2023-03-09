e5afdaeea823e120953e31be3b2c28f6371ba6d0
akira-yp <yog.puripuri@gmail.com>
2023-03-10 00:26:00 +0900

    管理画面メニューの雛形を作成

--- app/common/components/admin/admin-menu.tsx
+++ app/common/components/admin/admin-menu.tsx
@@ -1,17 +1,44 @@
 'use client'
 
 import React from 'react'
-import { Icon } from '@chakra-ui/react'
-import { FaGithub } from "react-icons/fa"
+import { IconButton } from '@chakra-ui/react'
+import { MdLibraryAdd, MdPlaylistAddCheck,MdAccessibilityNew } from "react-icons/md"
+import Link from 'next/link'
+
 
 const AdminMenu = () => {
   return (
     <div className='min-h-screen'>
-       <div className='flex justify-center'>
-      <Icon as={FaGithub} boxSize={10} className='flex-none' />
+      <div className='flex justify-center mt-5'>
+        <Link href="/admin/">
+          <IconButton
+            colorScheme='blue'
+            aria-label='show new'
+            icon={<MdPlaylistAddCheck/>}
+            fontSize='25px'
+            rounded='full'
+          />
+        </Link>
       </div>
-      <div className='flex justify-center'>
-        <Icon as={FaGithub} boxSize={10} className='flex-none' />
+      <div className='flex justify-center m-5'>
+        <Link href="/admin/new">
+          <IconButton
+            colorScheme='blue'
+            aria-label='show new'
+            icon={<MdLibraryAdd/>}
+            fontSize='25px'
+            rounded='full'
+          />
+        </Link>
+      </div>
+      <div className='flex justify-center m-5'>
+        <IconButton
+          colorScheme='blue'
+          aria-label='show new'
+          icon={<MdAccessibilityNew/>}
+          fontSize='25px'
+          rounded='full'
+        />
       </div>
     </div>
   )
